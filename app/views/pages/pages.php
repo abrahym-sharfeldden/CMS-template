8c7dea77a203ff011741e5d5a73d7addcfcaa6b5
yaboisharf <asharfeldden@gmail.com>
2017-03-21 19:53:14 -0400

    Added foreach in pages.php

--- app/views/pages/pages.php
+++ app/views/pages/pages.php
@@ -2,6 +2,18 @@
 
 <div class="container">
 	<a href="<?php echo BASE_URL . "/index.php"; ?>">Back to home</a>
+
+	<?php if (empty($pages)): ?>
+		<p> Nothing here at the moment. </p>
+	<?php else: ?>
+		<?php foreach($pages as $page): ?>
+			<tr>
+				<td><?php echo e($page['name']); ?></td>
+				<td><?php echo e($page['title']); ?></td>
+				<td><?php echo e($page['detail']); ?></td>
+			</tr>
+		<?php endforeach; ?>
+	<?php endif; ?>
 </div>
 
 <?php require VIEW_ROOT . '/templates/footer.php'; ?>
